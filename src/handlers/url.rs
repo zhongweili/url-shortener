e15ea99d2ab65ff17480941f6e8303bdc36ddc50
Zhongwei Li <zhongweili@tubi.tv>
2024-06-04 06:41:02 +0800

    refactor: Refactor URL record fields and database schema
    - Update field names in `UrlRecord`
    - Modify method parameters to use `id` instead of `short_url`
    - Adjust data type and column positions in initialization SQL script

--- src/handlers/url.rs
+++ src/handlers/url.rs
@@ -10,7 +10,7 @@ use axum_macros::debug_handler;
 use nanoid::nanoid;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
-use tracing::{info, warn};
+use tracing::{debug, warn};
 
 #[derive(Debug, Deserialize)]
 pub struct ShortenReq {
@@ -26,9 +26,9 @@ struct ShortenRes {
 #[derive(Debug, FromRow)]
 struct UrlRecord {
     #[sqlx(default)]
-    long_url: String,
+    id: String,
     #[sqlx(default)]
-    short_url: String,
+    url: String,
     #[sqlx(default)]
     clicks: i32,
 }
@@ -38,9 +38,9 @@ pub async fn shorten_handler(
     State(state): State<AppState>,
     Json(data): Json<ShortenReq>,
 ) -> Result<impl IntoResponse, StatusCode> {
-    let short_url = nanoid!(6);
+    let id = nanoid!(6);
     let url_record = state
-        .add_url_entry(data.url.as_str(), &short_url)
+        .add_url_entry(data.url.as_str(), &id)
         .await
         .map_err(|e| {
             warn!("got error {} when adding to db", e);
@@ -48,50 +48,49 @@ pub async fn shorten_handler(
         })?;
 
     let body = Json(ShortenRes {
-        url: format!("http://127.0.0.1:6688/{}", url_record.short_url),
+        url: format!("http://127.0.0.1:6688/{}", url_record.id),
         clicks: url_record.clicks,
     });
     Ok((StatusCode::CREATED, body))
 }
 
 pub async fn redirect_handler(
-    Path(short_url): Path<String>,
+    Path(id): Path<String>,
     State(state): State<AppState>,
 ) -> Result<impl IntoResponse, StatusCode> {
-    let url = state.get_url(&short_url).await.map_err(|e| {
+    let url = state.get_url(&id).await.map_err(|e| {
         warn!("got error {} when getting url", e);
         StatusCode::NOT_FOUND
     })?;
-    info!("get url: {} from: {}", url, short_url);
+    debug!("get url: {} from: {}", url, id);
     let mut headers = HeaderMap::new();
     headers.insert(LOCATION, url.parse().unwrap());
     Ok((StatusCode::PERMANENT_REDIRECT, headers))
 }
 
 impl AppState {
-    async fn add_url_entry(&self, url: &str, short_url: &str) -> Result<UrlRecord> {
-        let url_record: UrlRecord = sqlx::query_as(
-            "INSERT INTO urls (long_url, short_url) VALUES ($1, $2) RETURNING short_url",
-        )
-        .bind(url)
-        .bind(short_url)
-        .fetch_one(&self.pool)
-        .await?;
+    async fn add_url_entry(&self, url: &str, id: &str) -> Result<UrlRecord> {
+        let url_record: UrlRecord =
+            sqlx::query_as("INSERT INTO urls (id, url) VALUES ($1, $2) RETURNING id")
+                .bind(id)
+                .bind(url)
+                .fetch_one(&self.pool)
+                .await?;
         Ok(url_record)
     }
 
-    async fn get_url(&self, short_url: &str) -> Result<String> {
-        sqlx::query("UPDATE urls SET clicks = clicks + 1 WHERE short_url = $1")
-            .bind(short_url)
+    async fn get_url(&self, id: &str) -> Result<String> {
+        sqlx::query("UPDATE urls SET clicks = clicks + 1 WHERE id = $1")
+            .bind(id)
             .execute(&self.pool)
             .await?;
 
         let url_record: UrlRecord =
-            sqlx::query_as("SELECT long_url, short_url, clicks FROM urls WHERE short_url = $1")
-                .bind(short_url)
+            sqlx::query_as("SELECT id, url, clicks FROM urls WHERE id = $1")
+                .bind(id)
                 .fetch_one(&self.pool)
                 .await?;
 
-        Ok(url_record.long_url)
+        Ok(url_record.url)
     }
 }
